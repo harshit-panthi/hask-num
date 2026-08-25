e4451df7f43160a83fffe1917fc9ca30047c9ca7
Harshit Panthi <harshitpanthi2006@gmail.com>
2026-08-25 14:12:10 +0530

    refactor started

--- src/Nat.hs
+++ src/Nat.hs
@@ -5,57 +5,51 @@ module Nat where
 import Prelude hiding (Int, Real)
 import Numeric.Natural (Natural)
 import Data.Ratio ((%))
-import Data.List (scanl')
 
 type Nat = Natural
 type Int = Integer
 type Rat = Rational
 
-fact :: Nat -> Nat
-fact n = foldl' (*) 1 [1..n]
+-- first field is the min error
+data Real = RatR !Rat !Rat | LimR !Rat !(Rat -> Rat)
 
--- Reals are represented as Cauchy sequences
-data Real = Real !(Nat -> Rat) !(Rat -> Nat)
+-- | addReal wght a b, 0 < wght < 1, adds a and b, and if both a and b are
+-- stored as fuzzy approximators, then approximating (addReal w ...) to within
+-- eps aprroximates a within epsa + emme * w and b to within
+-- epsb + emme * (1 - w) (its better to add with weights 0 (or 1) when we know
+-- that the approximator of the first (or the second) number is a constant
+-- function (which we do when we know that its constructor is RatR))
+addReal :: Rat -> Real -> Real -> Real
+addReal _    (RatR epsa a) (RatR epsb b) = RatR (epsa + epsb) (a + b)
+addReal _    (RatR epsa a) (LimR epsb b) =
+  LimR (epsa + epsb) (\eps -> a + b (eps - epsa))
+addReal _    (LimR epsa a) (RatR epsb b) =
+  LimR (epsa + epsb) (\eps -> a (eps - epsb) + b)
+addReal wght (LimR epsa a) (LimR epsb b) =
+  LimR
+  (epsa + epsb)
+  (\eps ->
+      let emme = eps - epsa - epsb
+      in (a $ epsa + emme * wght) + (b $ epsb + emme * (1 - wght)))
+
+mulReal :: Rat -> Real -> Real -> Real
+mulReal _ (RatR epsa a) (RatR epsb b) =
+  RatR me centre where
+  me = centre - bot
+  centre = (top + bot) / 2
+  ends =
+    [ (a + epsa) * (b + epsb)
+    , (a + epsa) * (b - epsb)
+    , (a - epsa) * (b + epsb)
+    , (a - epsa) * (b - epsb)]
+  top = maximum ends
+  bot = minimum ends
+mulReal _ (RatR epsa a) (LimR epsb b) = LimR me centre where
+  me = undefined
+  centre eps = undefined
+mulReal _ _ _ = error "mulReal not implemented"
+
+-- | centre - a[epsa] * b[d] | < eps
 
 instance Num Real where
-  (Real a conva) + (Real b convb) =
-    Real (\n -> (a n) + (b n))
-    (\eps -> max (conva $ eps / 2) (convb $ eps / 2))
-    
-  (Real a conva) * (Real b convb) = Real (\n -> (a n) * (b n)) (\eps -> n' eps) where
-    m :: Rat -> Rat
-    m eps =
-      max
-      (foldl' (max) (0 % 1) $ ((1 % 1) + (a $ (conva eps) + 1)) : (map a [0..(conva eps)]))
-      (foldl' (max) (0 % 1) $ ((1 % 1) + (b $ (convb eps) + 1)) : (map b [0..(convb eps)]))
-
-    n' :: Rat -> Nat
-    n' eps = max (conva $ eps / ((2 % 1) * (m eps))) (convb $ eps / ((2 % 1) * (m eps)))
-    
-  abs (Real a conva) = Real (abs . a) conva
-  
-  negate (Real a conva) = Real (negate . a) conva
-
-  signum (Real a _) = Real (signum . a)
-    (error "dont use the signum of (Num Real), use sgn instead")
-
-  fromInteger n = Real (\_ -> (n % 1)) (\_ -> 0)
-
-approx :: Rat -> Real -> Rat
-approx eps (Real a conva) = a $ conva (eps / 2) 
-
-factorials :: [Integer]
-factorials = scanl' (*) 1 [1..]
-
-e_taylor_term :: [Rat]
-e_taylor_term = map (\n -> 1 / fromIntegral n) factorials
-
-e_taylor_series_list :: [Rat]
-e_taylor_series_list = scanl' (+) 0 (e_taylor_term)
-
-e_taylor_series_seq :: Nat -> Rat
-e_taylor_series_seq n = e_taylor_series_list !! fromIntegral n
-
-e :: Real
-e = Real e_taylor_series_seq (\eps -> max 1 ((ceiling $ 1 / eps) - 1))
-
+  a + b = addReal (1 % 2) a b
